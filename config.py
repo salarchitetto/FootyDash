d8c0669f899f438f57af970bb93f365c3762b4fa
Salvatore Architetto <sal.architetto862@gmail.com>
2019-09-29 22:04:28 -0400

    updated config..

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #AWS INFO
-KEY = 'AKIARTK5WYTWWQ7GTL7R'
-SECRET = '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'
-BUCKET_NAME = 'footydashdata'
+KEY = ''
+SECRET = ''
+BUCKET_NAME = ''
 
-file_path = 'C:\\Users\\Sal Architetto\\Desktop\\'
-s3_path = 'C:\\Users\\Sal Architetto\\Desktop\\footy_data_sets'
+file_path = ''
+s3_path = ''
 
 #SCRAPER INFO
 chromeDriver = r'C:\Users\Sal Architetto\Downloads\chromedriver\chromedriver_win32\chromedriver.exe'
